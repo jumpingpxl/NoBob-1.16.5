25e57345e5c10069fc0950c31456d123a22ec109
JumpingPxl <jumpingpxl@gmail.com>
2021-05-17 18:21:48 +0200

    Add addon specific settings

--- src/main/java/de/jumpingpxl/labymod/nobob/util/Settings.java
+++ src/main/java/de/jumpingpxl/labymod/nobob/util/Settings.java
@@ -2,8 +2,11 @@ package de.jumpingpxl.labymod.nobob.util;
 
 import com.google.gson.JsonObject;
 import de.jumpingpxl.labymod.nobob.NoBob;
+import net.labymod.settings.elements.BooleanElement;
+import net.labymod.settings.elements.ControlElement;
 import net.labymod.settings.elements.HeaderElement;
 import net.labymod.settings.elements.SettingsElement;
+import net.labymod.utils.Material;
 
 import java.util.List;
 
@@ -11,16 +14,30 @@ public class Settings {
 
 	private final NoBob noBob;
 
+	private boolean enabled;
+
 	public Settings(NoBob noBob) {
 		this.noBob = noBob;
 	}
 
 	public void loadConfig() {
-
+		enabled = !getConfig().has("enabled") || getConfig().get("enabled").getAsBoolean();
 	}
 
 	public void fillSettings(List<SettingsElement> settingsElements) {
 		settingsElements.add(new HeaderElement("§eNoBob v" + NoBob.VERSION));
+		settingsElements.add(new HeaderElement(""));
+		settingsElements.add(
+				new BooleanElement("§6Enabled", new ControlElement.IconData(Material.LEVER), newValue -> {
+					enabled = newValue;
+					getConfig().addProperty("enabled", newValue);
+					saveConfig();
+				}, enabled));
+		settingsElements.add(new HeaderElement(""));
+		settingsElements.add(new HeaderElement("§4§lIMPORTANT"));
+		settingsElements.add(new HeaderElement(
+				"§cThe following setting has to be enabled in order for NoBob to work"));
+		settingsElements.add(new HeaderElement("§cOptions -> Video Settings -> View Bobbing"));
 	}
 
 	private JsonObject getConfig() {
